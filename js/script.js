548d6b8c59525e87de5e396cf9aab3a2a0aa3591
lDonKaban <51529534+lDonKaban@users.noreply.github.com>
2023-05-16 06:54:55 +0300

    Update time

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
 
     tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
-    timer('.timer', '2021-07-22');
+    timer('.timer', '2030-07-22');
     cards();
     calc();
     form('form', modalTimerId);
